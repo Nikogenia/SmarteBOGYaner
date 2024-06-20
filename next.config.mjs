7ca235253c74144f2e89761d0ddcd44e160dd4d2
Nikogenia <info@nikogenia.de>
2024-06-20 19:40:46 +0200

    Optimized build toolchain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+let dataUrl = "/data/";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    DATA_URL: dataUrl,
+    COVER_URL: dataUrl + "covers/",
+    THUMBNAIL_URL: dataUrl + "thumbnails/",
+    VIDEO_URL: dataUrl + "videos/",
+    PORTRAIT_URL: dataUrl + "portraits/",
+  }
+};
 
 export default nextConfig;
